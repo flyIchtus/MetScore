53aabc1269eed97bc04e94d07ab630257dcc8854
flyIchtus <clement.brochet@meteo.fr>
2024-03-13 09:59:03 +0100

    fix rank histogram

--- metrics/rank_histogram.py
+++ metrics/rank_histogram.py
@@ -21,8 +21,7 @@ def rank_histo(obs_data, fake_data):
         
     Returns :
         
-        bins (C, 121) max number of members in the ensemble  
-    
+        bins (C, N + 1)
     """
     
     N, C, H, W  = fake_data.shape
@@ -30,15 +29,14 @@ def rank_histo(obs_data, fake_data):
     fake_data_p = copy.deepcopy(fake_data)
     obs_data_p = copy.deepcopy(obs_data)
 
-    bins = np.zeros((C, 121)) # 121 since N=120 is the biggest ensemble... Maybe use the biggest number that we will use 1000? 
+    bins = np.zeros((C, fake_data.shape[0] + 1))
     for i in range(C):
         
-        obs_data_var = copy.deepcopy(obs_data_p[i])
-        fake_data_var = copy.deepcopy(fake_data_p[:,i])
-        
-        obs = obs_data_var[ ~np.isnan(obs_data_var)]
-        ens = fake_data_var[:, ~np.isnan(obs_data_var)]
-        
+        obs_data_var = obs_data_p[i]
+        fake_data_var = fake_data_p[:,i]
+   
+        obs = obs_data_var[~np.isnan(obs_data_var)]
+        ens = fake_data_var[:,~np.isnan(obs_data_var)]
 
         for j in range(obs.shape[0]):
         
@@ -46,46 +44,30 @@ def rank_histo(obs_data, fake_data):
             ens_sort.sort()
             ens_sort = np.concatenate((ens_sort, [9999999.]))
             out = np.where((ens_sort < obs[j]), True, False)
-            bins[i, np.argmax(out==False)] +=1 
+            bins[i, np.argmax(out==False)] += 1 / obs.shape[0]
         
     return bins
 
-def unreliability(obs_data, fake_data):
+
+def unreliability(rankHisto):
     """
-    
     Inputs :
         
-        fake_data : N x C x H x W array with N samples
-        
-        obs_data : C x H x W array observation
+        rankHisto :  C x N_bins array with C different variables    
         
     Returns :
         
-        bins (C, 121) max number of members in the ensemble  
-    
+        Delta :  (C,) shaped array, estimation of sum(n - Delta0)**2 for each bin n onf rankHisto
+        Delta0 : flot, the flat histogram value
+        unreliability : (C,) shaped array, Delta / Delta0
     """
-    
-    N, C, H, W  = fake_data.shape
-    
-    fake_data_p = copy.deepcopy(fake_data)
-    obs_data_p = copy.deepcopy(obs_data)
+    C, N_bins = rankHisto.shape
 
-    bins = np.zeros((C, 121)) # 121 since N=120 is the biggest ensemble... Maybe use the biggest number that we will use 1000? 
-    for i in range(C):
-        
-        obs_data_var = copy.deepcopy(obs_data_p[i])
-        fake_data_var = copy.deepcopy(fake_data_p[:,i])
-        
-        obs = obs_data_var[ ~np.isnan(obs_data_var)]
-        ens = fake_data_var[:, ~np.isnan(obs_data_var)]
-        
+    delta0 = 1 / (N_bins)
 
-        for j in range(obs.shape[0]):
-        
-            ens_sort = ens[:,j]
-            ens_sort.sort()
-            ens_sort = np.concatenate((ens_sort, [9999999.])) #ensuring there's at least one point > obs 
-            out = np.where((ens_sort < obs[j]), True, False)
-            bins[i, np.argmax(out==False)] +=1 
-        
-    return bins
+    delta = np.zeros((C,))
+
+    for var_idx in range(C):
+        delta[var_idx] = ((rankHisto[var_idx,:] - delta0) ** 2).sum()
+
+    return delta, delta0, delta / delta0
